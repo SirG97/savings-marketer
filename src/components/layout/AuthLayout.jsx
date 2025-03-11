f2ef8836b716246a1385a69bcf3fdde5751bca07
Nedu Backend <nedubackend@Nedus-MacBook-Pro.local>
2025-03-11 09:47:28 +0100

    added logo

--- src/components/layout/AuthLayout.jsx
+++ src/components/layout/AuthLayout.jsx
@@ -1,4 +1,5 @@
 import { Toaster } from "sonner"
+import logo from "../../assets/logos/profile.png"
 export default function AuthLayout({ children, header }) {
   return (
     <>
@@ -8,10 +9,10 @@ export default function AuthLayout({ children, header }) {
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <img
             alt="Your Company"
-            src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-            className="mx-auto h-10 w-auto"
+            src={logo}
+            className="mx-auto h-8"
           />
-          <h2 className="mt-3 text-center text-2xl font-bold tracking-tight text-gray-900">
+          <h2 className=" mt-2 text-center text-2xl font-bold tracking-tight text-gray-900">
             Marketer
           </h2>
           <h2 className="mt-2 text-center text-lg/9 font-bold tracking-tight text-gray-900">
